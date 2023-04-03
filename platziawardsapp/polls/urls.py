bad0453980884a5598958bcac563e3fa0c746641
Jonathan Villegas <jodaviza@gmail.com>
2023-04-02 23:52:11 -0300

    View and templates finished

--- platziawardsapp/polls/urls.py
+++ platziawardsapp/polls/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('details/<int:question_id>', views.detail, name='detail'),
-    path('results/<int:question_id>', views.results, name='results'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('details/<int:pk>', views.DetailView.as_view(), name='detail'),
+    path('results/<int:pk>', views.ResultsView.as_view(), name='results'),
     path('vote/<int:question_id>', views.vote, name='vote')
 ]
